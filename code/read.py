850fbbbbfe5cad40ca0f62df0b70cad7552598b4
iveyj <ivey@altair.com>
2022-07-17 12:20:14 -0400

    initial commit - original read5.py from 2019

---
+++ code/read.py
@@ -0,0 +1,264 @@
+import numpy as np
+import matplotlib.animation as animation
+import time
+import serial
+import tkinter as tk
+import tkinter.ttk as ttk
+from tkinter.filedialog import askopenfilename
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg, NavigationToolbar2Tk)
+# Implement the default Matplotlib key bindings.
+from matplotlib.figure import Figure
+import os
+import configparser
+
+def mb(s):
+    tk.messagebox.showinfo(" ", s)
+
+class StdoutRedirector(object):
+    def __init__(self,text_widget):
+        self.text_space = text_widget
+
+    def write(self,string):
+        self.text_space.insert('end', string)
+        self.text_space.see('end')
+        
+    def flush(self):
+    	pass
+    
+#shift a numpy array right, dropping last n (or shift left and drop first n if n negative) and fill with NaN
+def shift(xs, n):
+    e = np.empty_like(xs)
+    if n >= 0:
+        e[:n] = np.nan
+        e[n:] = xs[:-n]
+    else:
+        e[n:] = np.nan
+        e[:n] = xs[-n:]
+    return e
+
+class PlotLogGui:
+    def __init__(self):
+
+        self.master = tk.Tk()
+        
+        self.style = ttk.Style()     
+        self.style.configure('TButton', anchor='center')
+        self.style.configure('TLabel', anchor='center')
+        self.style.configure('TNotebook.Tab', foreground='blue')
+        self.style.configure('TLabelframe.Label', foreground='blue')
+  
+        # create main window
+        self.master.title("Record - Version 0.0")
+        self.master.geometry('800x600')
+        self.master.rowconfigure(0,weight=1)
+        self.master.columnconfigure(0, weight=1)
+
+        # create outermost frame (surrounds everything)
+        self.frame = tk.Frame(self.master)
+        self.frame.config(borderwidth=2, relief="sunken")
+        self.frame.grid(column=0, row=0, sticky='NSEW')
+
+        # create frame for bottom text widget
+        self.textFrame = tk.Frame(self.frame)
+        # create text widget and scrollbar
+        self.text = tk.Text(self.textFrame, borderwidth=2, relief="sunken", width=100, height=12, )
+        self.text.config(font=("consolas",8))
+        self.scroll = tk.Scrollbar(self.textFrame, command=self.text.yview)
+        self.text['yscrollcommand'] = self.scroll.set   
+
+        # create plot widget
+        self.fig = Figure()
+        self.fig.set_tight_layout(True)
+        
+        # create canvas for figure to draw in using Agg backend
+        self.plotcanvas = FigureCanvasTkAgg(self.fig, master=self.frame)  
+        self.toolbarFrame = tk.Frame(self.frame)
+
+        # create frame for left-side controls
+        self.controlFrame = tk.Frame(self.frame)
+        self.controlFrame.config(borderwidth=2, relief="sunken")
+        
+        # create label
+        self.readbutton = ttk.Button(self.controlFrame, text="Read data", command=self.read_data)
+
+        # configure frame grid
+        colwt = [0,1]
+        for i in range(0,len(colwt)):
+            self.frame.columnconfigure(i, weight=colwt[i])
+        rowwt = [1,0]
+        for i in range(0,len(rowwt)):
+             self.frame.rowconfigure(i, weight=rowwt[i])
+
+        # configure textFrame grid
+        self.textFrame.grid_rowconfigure(0, weight=1)
+        self.textFrame.grid_columnconfigure(0, weight=1)
+        
+        # lay out items
+        self.toolbarFrame.grid(row=1, column=1,sticky=('W'))
+        # textFrame in frame
+        self.textFrame.grid(row=2, column=1,  sticky=('SEW'))
+        # text and scroll in textFrame
+        self.text.grid(row=0,column=0,sticky="nsew",padx=2,pady=2)
+        self.scroll.grid(row=0,column=1,sticky="nsew")
+        
+        # canvas in frame
+        self.plotcanvas.get_tk_widget().grid(row=0, column=1,  sticky=('NSEW'))
+        toolbar = NavigationToolbar2Tk(self.plotcanvas, self.toolbarFrame)
+        toolbar.update()
+        
+        # controlframe in frame
+        self.controlFrame.grid(row=0, column=0, rowspan=2)
+
+        # label in controlframe
+        self.readbutton.grid(row=0, column=0)
+
+        self.filter = None
+        self.axs = self.fig.add_subplot(111)
+        
+
+        self.strip_time = 3600.0 
+        self.sample_rate = 10.0
+        self.strip_samples = int(self.strip_time * self.sample_rate)
+
+        self.trace = {"t" : np.linspace(899.9, 0, 9000), "y" : np.zeros(9000)}
+        self.lines = []
+        self.i = 0
+
+        self.config = configparser.ConfigParser()
+        try:
+            with open('read_config.ini', 'r') as configfile:
+                self.config.read_file(configfile)
+        except IOError:
+            self.config["DEFAULT"] = {'com_port' : 'COM1', 'baud_rate' : '115200', 'out_file_base_name' : "unknown"}
+            with open('read_config.ini', 'w') as configfile:
+                self.config.write(configfile)
+        self.port = self.config["DEFAULT"]["com_port"]
+        self.baudrate = self.config["DEFAULT"]["baud_rate"]
+
+        self.out_file_name = self.config["DEFAULT"]["out_file_base_name"] + time.strftime("%Y-%m-%dT%H%M", time.gmtime()) +'.csv' 
+        self.out_file = open(self.out_file_name, "w")
+        print(type(self.out_file))
+        print("opening: %s" % os.path.realpath(self.out_file.name))
+
+
+        # ports = list_ports.comports()
+        # portstrings = []
+        # # for port in ports:
+        #     portstrings.append(port.device)
+        #     ret = dialog.Dialog(title = "Select com port",  text = "foo", bitmap='questhead', default = 0, strings = portstrings)
+        #sys.stdout = StdoutRedirector(self.text)
+        #print("Opening ", portstrings[ret.num])
+        #print("Opening ", )
+        self.ser = serial.Serial(self.port, self.baudrate, timeout = 0)
+        while(self.ser.read(1000) == 1000):
+            pass
+        self.sampcount = 0
+        self.remainder = ""
+              
+    def read_data(self):
+        str = self.ser.read(1000).decode("utf-8")
+        if len(str) > 0:
+            #print(self.sampcount, str)
+            list, self.remainder = getlines(self.remainder+str)
+            #print(list, self.remainder)
+            for data in list:
+                self.sampcount = self.sampcount + 1
+                self.trace["y"] = shift(self.trace['y'], -1)
+                self.trace["y"][-1] = int(data)
+            if self.out_file != None:
+                for value in list:
+                    self.out_file.write(value + "\n") 
+            self.out_file.close()
+            self.out_file = open(self.out_file_name, "a")
+            self.plot()
+        #self.master.after(1000, self.read_data())
+   
+    def plot(self):
+        if len(self.lines) == 0:
+            self.axs.set_ylim(100000,-100000)
+            self.axs.set_xlim(900,0)
+            self.lines = self.axs.plot(self.trace['t'], self.trace['y'], color = 'b', linewidth = 1)
+        else:
+            self.lines[0].set_data(self.trace['t'], self.trace['y'])
+        self.fig.canvas.draw_idle()
+
+
+import ctypes
+from ctypes import wintypes
+import os
+import msvcrt
+
+GENERIC_READ = 0x80000000
+GENERIC_WRITE = 0x40000000
+
+OPEN_EXISTING = 3
+OPEN_ALWAYS = 4
+
+ACCESS_MODES = {
+    "r": GENERIC_READ,
+    "w": GENERIC_WRITE,
+    "r+": (GENERIC_READ|GENERIC_WRITE)
+}
+
+OPEN_MODES = {
+    "r": OPEN_EXISTING,
+    "w": OPEN_ALWAYS,
+    "r+": OPEN_ALWAYS,
+}
+
+
+def open_file_nonblocking(filename, access):
+    # Removes the b for binary access.
+    internal_access = access.replace("b", "")
+    access_mode = ACCESS_MODES[internal_access]
+    open_mode = OPEN_MODES[internal_access]
+    handle = wintypes.HANDLE(ctypes.windll.kernel32.CreateFileW(
+        wintypes.LPWSTR(filename),
+        wintypes.DWORD(access_mode),
+        wintypes.DWORD(2|1),  # File share read and write
+        ctypes.c_void_p(0),
+        wintypes.DWORD(open_mode),
+        wintypes.DWORD(0),
+        wintypes.HANDLE(0)
+    ))
+
+    try:
+        fd = msvcrt.open_osfhandle(handle.value, 0)
+    except OverflowError as exc:
+        # Python 3.X
+        raise OSError("Failed to open file.") from None
+        # Python 2
+        # raise OSError("Failed to open file.")
+
+    return os.fdopen(fd, access)
+
+def getlines(str):
+    list = []
+    partial = ""
+    #print("in getlines", str)
+    for i in range(len(str)):
+        if(str[i] == '\r' or str[i] == '\n'):
+            if(len(partial) > 0):
+                #print("partial", partial)
+                list.append(partial)
+                partial = ""
+        else:
+            #print(str[i])
+            partial = partial + str[i]
+    #print("result", list, partial)
+    return(list, partial)        
+
+def update(a):
+    global gui
+    gui.read_data()
+
+i = 0               
+gui = PlotLogGui()
+anim = animation.FuncAnimation(gui.fig, update, interval=100)
+#gui.read_data()
+gui.master.mainloop()
+gui.ser.close()
+if gui.out_file != None:
+    gui.out_file.close()
+
